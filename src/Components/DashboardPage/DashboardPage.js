0daf9c0c24f7dad8894b6e3f532be9ea8a57c3e4
Callasaurus <c.sullivandrage@outlook.com>
2023-01-09 14:17:58 +0000

    starting on the dashboard

--- src/Components/DashboardPage/DashboardPage.js
+++ src/Components/DashboardPage/DashboardPage.js
@@ -3,6 +3,7 @@ import { Button, Alert } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
 import Navbar from '../Navbar/Navbar'
+import './DashboardPage.css'
 
 export default function DashboardPage() {
     const [error, setError] = useState('')
@@ -32,20 +33,37 @@ export default function DashboardPage() {
         navigate('/portfolio', { replace: true })
     }
 
-    return (
-        <>
-        <Navbar/>
+    return [
+        <Navbar />,
+        <div className='dashboard-page-cont'>
             {error && <Alert variant='danger'>{error}</Alert>}
 
-            <h1> buttons below will be the cards for the components </h1>
-            <button onClick={navigateApplications}> My applications</button>
-            <button onClick={navigateInterviewPrep}> Interview Prep</button>
-            <button onClick={navigatePortfolio}> Perfect your portfolio</button>
+            <h1>Everything you need for job preparation in one place</h1>
 
+            <div className='dashboard-cards'>
+                <a className='dashboard-card' onClick={navigateApplications}>
+                    <h1> Interview Prep </h1>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Sea_Otter_%28Enhydra_lutris%29_%2825169790524%29_crop.jpg/800px-Sea_Otter_%28Enhydra_lutris%29_%2825169790524%29_crop.jpg"/>
+                    <p>Find all the essential knowledge and material you need for a junior developer interview</p>
+                </a>
+
+                <a className='dashboard-card' onClick={navigateInterviewPrep}>
+                    <h1> My applications </h1>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Sea_Otter_%28Enhydra_lutris%29_%2825169790524%29_crop.jpg/800px-Sea_Otter_%28Enhydra_lutris%29_%2825169790524%29_crop.jpg"/>
+                    <p>Track all of your job applications easily and in one place </p>
+                </a>
+
+                <a className='dashboard-card' onClick={navigatePortfolio}>
+                    <h1> Perfect your portfolio </h1>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Sea_Otter_%28Enhydra_lutris%29_%2825169790524%29_crop.jpg/800px-Sea_Otter_%28Enhydra_lutris%29_%2825169790524%29_crop.jpg"/>
+                    <p>Learn all you need to perfect your portfolio before you apply</p>
+                </a>
+
+            
+            </div>
             <div className='w-100 text-center mt-2'>
                 <Button variant='link' onClick={handleLogout}>Log Out</Button>
             </div>
-        </>
-
-    )
+        </div>
+    ]
 }
