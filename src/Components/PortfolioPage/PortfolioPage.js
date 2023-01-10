4c5adcf2686cf90dd70726d656a69102c5c6f86c
constantine30 <banciu.constantin@gmail.com>
2023-01-10 10:07:35 +0000

    testing for MiKo

--- src/Components/PortfolioPage/PortfolioPage.js
+++ src/Components/PortfolioPage/PortfolioPage.js
@@ -4,7 +4,7 @@ import Navbar from '../Navbar/Navbar'
 function PortfolioPage (props){
     return [
     <Navbar/>,
-    <h1>This is the Portfolio page</h1>
+    <h1>This is the Portfolio page and a test for MiKo!</h1>
     ]
 }
 export default PortfolioPage
