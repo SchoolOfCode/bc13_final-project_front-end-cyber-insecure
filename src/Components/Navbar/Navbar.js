9b9b5683872d77f7cf04bfad61338ad4f6396708
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2023-01-10 16:33:57 +0100

    hamburger navbar css done

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,54 +1,57 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import './Navbar.css'
-import {useState} from "react" 
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Navbar.css";
+import { useState } from "react";
 
 export default function Navbar() {
-    const [hamburger, setHamburger] = useState(false)
+  const [hamburger, setHamburger] = useState(true);
 
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const navigateHome = () => {
+    navigate("/", { replace: true });
+  };
 
-    const navigateHome = () => {
-        navigate('/', { replace: true })
-    }
+  const navigateApplications = () => {
+    navigate("/applications", { replace: true });
+  };
 
-    const navigateApplications = () => {
-        navigate('/applications', { replace: true })
-    }
+  const navigateInterviewPrep = () => {
+    navigate("/interviewprep", { replace: true });
+  };
 
-    const navigateInterviewPrep = () => {
-        navigate('/interviewprep', { replace: true })
-    }
+  const navigateCV = () => {
+    navigate("/cv-workshop", { replace: true });
+  };
 
-    const navigateCV = () => {
-        navigate('/cv-workshop', { replace: true })
-    }
+  const navigateKnowledgebank = () => {
+    navigate("/knowledge-bank", { replace: true });
+  };
 
-    const navigateKnowledgebank = () => {
-        navigate('/knowledge-bank', { replace: true })
-    }
+  const navigateQuizzes = () => {
+    navigate("/quizzes", { replace: true });
+  };
 
-    const navigateQuizzes = () => {
-        navigate('/quizzes', { replace: true })
-    }
+  const navigatePortfolio = () => {
+    navigate("/portfolio", { replace: true });
+  };
 
-    const navigatePortfolio = () => {
-        navigate('/portfolio', { replace: true })
-    }
-
-    return  (
-        <div className="navbar-container">
-        <div className="hamburger"><button onClick={() => setHamburger(!hamburger)} id="hamburger">Open</button></div>
-        <div className='navbar-links'>
-            <button onClick={navigateHome}> Dashboard </button>
-            <button onClick={navigateApplications}> My applications</button>
-            <button onClick={navigateInterviewPrep}> Interview Prep</button>
-            <button onClick={navigatePortfolio}> Perfect your portfolio</button>
-            <button onClick={navigateCV}> CV workshop </button>
-            <button onClick={navigateKnowledgebank}> Knowledge Bank </button>
-            <button onClick={navigateQuizzes}> Quizzes </button>
-        </div>
-        </div>
-    ) 
+  return (
+    <div className={hamburger?"navbar-container":"navbar-open"}>
+      <div className="hamburger">
+        <button onClick={() => setHamburger(!hamburger)} id="hamburger">
+          Open
+        </button>
+      </div>
+      <div className={hamburger?"navbar-links":"hamburger-open"}>
+        <button onClick={navigateHome}> Dashboard </button>
+        <button onClick={navigateApplications}> My applications</button>
+        <button onClick={navigateInterviewPrep}> Interview Prep</button>
+        <button onClick={navigatePortfolio}> Perfect your portfolio</button>
+        <button onClick={navigateCV}> CV workshop </button>
+        <button onClick={navigateKnowledgebank}> Knowledge Bank </button>
+        <button onClick={navigateQuizzes}> Quizzes </button>
+      </div>
+    </div>
+  );
 }
