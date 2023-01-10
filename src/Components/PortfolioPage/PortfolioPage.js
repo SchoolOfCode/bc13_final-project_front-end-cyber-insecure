6cf02838e16c53a95c5e5ce6c276c99bb1948b10
Callasaurus <c.sullivandrage@outlook.com>
2023-01-10 14:20:41 +0000

    working on pop ups for portfolio

--- src/Components/PortfolioPage/PortfolioPage.js
+++ src/Components/PortfolioPage/PortfolioPage.js
@@ -2,20 +2,39 @@ import React from "react";
 import Popup from "../Popup/Popup";
 import Navbar from '../Navbar/Navbar'
 import { useState } from "react";
+import jordanGithubPlain from '../../Images/jordanGithubPlain.png'
+import jordanGithubPopup from '../../Images/jordanGithubPopup.png'
+import './PortfolioPage.css'
 
-function PortfolioPage (props){
+function PortfolioPage(props) {
 
     const [buttonPopUp, setButtonPopUp] = useState(false);
 
     return [
-    <Navbar/>,
-    <h1>This is the Portfolio page</h1>,
-    
-    <a onClick={() => setButtonPopUp(true)}>Answer Example
-        <Popup trigger={buttonPopUp} setTrigger={setButtonPopUp}>
-        <h3>Portfolio Page example</h3>
-    </Popup>
-    </a>
+        <Navbar />,
+        <div className='PortfolioPage-container'>
+        <h1>This is the Portfolio page</h1>
+
+            <div className='PortfolioPage-examples'>
+                <div className="PorfolioPage-example-item">
+                    <a onClick={() => setButtonPopUp(true)}>
+                        <img className='portfolio-plain-image' src={jordanGithubPlain} alt='' />
+                        <Popup trigger={buttonPopUp} setTrigger={setButtonPopUp}>
+                            <img className='portfolio-popup-image' src={jordanGithubPopup} alt='' />
+                        </Popup>
+                    </a>
+                </div>
+
+                <div className="PorfolioPage-example-item">
+                    <a onClick={() => setButtonPopUp(true)}>
+                        <img className='portfolio-plain-image' src={jordanGithubPlain} alt='' />
+                        <Popup trigger={buttonPopUp} setTrigger={setButtonPopUp}>
+                            <img img className='portfolio-popup-image' src={jordanGithubPopup} alt='' />
+                        </Popup>
+                    </a>
+                </div>
+            </div>
+        </div>
     ]
 }
 export default PortfolioPage
