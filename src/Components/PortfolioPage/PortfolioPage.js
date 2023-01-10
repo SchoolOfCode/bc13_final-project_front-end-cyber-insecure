b2cb070107f6ad2813a930b37818751bbecc7644
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-01-10 12:23:08 +0000

    First commit of portfolioPrepPage, Taylor and Calla

--- src/Components/PortfolioPage/PortfolioPage.js
+++ src/Components/PortfolioPage/PortfolioPage.js
@@ -1,10 +1,21 @@
 import React from "react";
+import Popup from "../Popup/Popup";
 import Navbar from '../Navbar/Navbar'
+import { useState } from "react";
 
 function PortfolioPage (props){
+
+    const [buttonPopUp, setButtonPopUp] = useState(false);
+
     return [
     <Navbar/>,
-    <h1>This is the Portfolio page</h1>
+    <h1>This is the Portfolio page</h1>,
+    
+    <a onClick={() => setButtonPopUp(true)}>Answer Example
+        <Popup trigger={buttonPopUp} setTrigger={setButtonPopUp}>
+        <h3>Portfolio Page example</h3>
+    </Popup>
+    </a>
     ]
 }
 export default PortfolioPage
