f44469a1971f87540f95356b302ef3214f8f8d97
Callasaurus <c.sullivandrage@outlook.com>
2023-01-11 15:20:44 +0000

    knowledge bank component set up with dummy data

---
+++ src/Components/KnowledgeBankList/KnowledgeBankItem.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './KnowledgeBank.css'
+import Popup from 'reactjs-popup';
+
+export default function KnowledgeBankItem(props) {
+    return (
+        <div className='knowledge-bank-item'>
+            <div className='knowledge-bank-first-third'>
+                <h2> {props.title}</h2>
+                <Popup trigger={<img src={props.image} alt='' />}>
+                    <img className='knowledge-pop-up' src={props.image} alt='' />
+                </Popup>
+            </div>
+            <div className='knowledge-bank-middle-third'>
+                <h2> Definition </h2>
+                <p> {props.definition}</p>
+            </div>
+            <div className='knowledge-bank-last-third'>
+                <h2> Resource links </h2>
+                <a href={props.article_link_one} target="_blank" rel="noreferrer"> Article example 1</a>
+                <a href={props.article_link_two} target="_blank" rel="noreferrer"> Article example 2</a>
+                <a href={props.video_link} target="_blank" rel="noreferrer"> Video example </a>
+            </div>
+        </div>
+    )
+}
---
+++ src/Components/KnowledgeBankList/KnowledgeDummyData.js
@@ -0,0 +1,162 @@
+export const KnowledgeBank =   [
+    {
+        "title": "Array",
+        "definition": "An array is a collection of elements of the same type placed in contiguous memory locations that can be individually referenced by using an index to a unique identifier. Five values of type int can be declared as an array without having to declare five different variables (each with its own identifier).",
+        "image": "https://i.redd.it/s8ev4pw1p4a61.jpg",
+        "article_link_one": "https://www.w3schools.com/js/js_arrays.asp",
+        "article_link_two": "https://www.w3schools.com/js/js_arrays.asp",
+        "video_link": "https://www.youtube.com/watch?v=R8rmfD9Y5-c"
+    },
+    {
+        "title": "Arrow function",
+        "definition": "An arrow function expression is a compact alternative to a traditional function expression, with some semantic differences and deliberate limitations in usage: Arrow functions don't have their own bindings to this , arguments , or super , and should not be used as methods",
+        "image": "https://miro.medium.com/max/1400/1*_IejCVtnJg24RDNQC9xftg.png",
+        "article_link_one": "https://www.w3schools.com/js/js_arrow_function.asp",
+        "article_link_two": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions",
+        "video_link": "https://www.youtube.com/watch?v=h33Srr5J9nY"
+    },
+    {
+        "title": "Async function",
+        "definition": "The async function declaration declares an async function where the await keyword is permitted within the function body. The async and await keywords enable asynchronous, promise-based behavior to be written in a cleaner style, avoiding the need to explicitly configure promise chains.",
+        "image": "https://cdn.programiz.com/sites/tutorial2program/files/async-await-function.png",
+        "article_link_one": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function",
+        "article_link_two": "https://www.w3schools.com/js/js_async.asp",
+        "video_link": "https://www.youtube.com/watch?v=li7FzDHYZpc"
+    },
+    {
+        "title": "Database",
+        "definition": "In computing, a database is an organized collection of data stored and accessed electronically. Small databases can be stored on a file system, while large databases are hosted on computer clusters or cloud storage.",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7hH9pzWkAcEuhtzgBUBkURpCPiioiqAl0jw&usqp=CAU",
+        "article_link_one": "https://www.techtarget.com/searchdatamanagement/definition/database",
+        "article_link_two": "https://www.oracle.com/uk/database/what-is-database/",
+        "video_link": "https://www.youtube.com/watch?v=Zc14ZkWVhhs"
+    },
+    {
+        "title": "Document Object Model (DOM)",
+        "definition": "The Document Object Model is a cross-platform and language-independent interface that treats an XML or HTML document as a tree structure wherein each node is an object representing a part of the document. The DOM represents a document with a logical tree.",
+        "image": "https://www.w3schools.com/js/pic_htmltree.gif",
+        "article_link_one": "https://www.w3schools.com/js/js_htmldom.asp",
+        "article_link_two": "https://en.wikipedia.org/wiki/Document_Object_Model",
+        "video_link": "https://www.youtube.com/watch?v=ipkjfvl40s0"
+    },
+    {
+        "title": "End to end testing",
+        "definition": "End to end testing (E2E testing) is a software testing method that involves testing an application's workflow from beginning to end. This method aims to replicate real user scenarios to validate the system for integration and data integrity.",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFhjXHhNUo_8tbD3tafLxTe2Gxln5xS-Y8vA&usqp=CAU",
+        "article_link_one": "https://circleci.com/blog/what-is-end-to-end-testing/",
+        "article_link_two": "https://www.browserstack.com/guide/end-to-end-testing#:~:text=End%20to%20end%20testing%20(E2E,for%20integration%20and%20data%20integrity.",
+        "video_link": "https://www.youtube.com/watch?v=wycIcpuLkzU"
+    },
+    {
+        "title": "Event Listener",
+        "definition": "An event listener is a procedure in JavaScript that waits for an event to occur. The simple image of an event is a user clicking the mouse or pressing a key on the keyboard.",
+        "image": "https://bootcamp.burlingtoncodeacademy.com/images/event-listener.png",
+        "article_link_one": "https://www.w3schools.com/js/js_htmldom_eventlistener.asp",
+        "article_link_two": "https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener",
+        "video_link": "https://www.youtube.com/watch?v=XF1_MlZ5l6M"
+    },
+    {
+        "title": "For loops",
+        "definition": "A 'For' Loop is used to repeat a specific block of code a known number of times. For example, if we want to check the grade of every student in the class, we loop from 1 to that number. When the number of times is not known before hand, we use a 'While' loop.",
+        "image": "https://miro.medium.com/max/1400/1*Wal8vgWVHiYnM0IrnK0p3w.jpeg",
+        "article_link_one": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for",
+        "article_link_two": "https://www.w3schools.com/js/js_loop_for.asp",
+        "video_link": "https://www.youtube.com/watch?v=x7Xzvm0iLCI"
+    },
+    {
+        "title": "Function",
+        "definition": "A function in JavaScript is similar to a procedure—a set of statements that performs a task or calculates a value, but for a procedure to qualify as a function, it should take some input and return an output where there is some obvious relationship between the input and the output. A JavaScript function is executed when \"something\" invokes it (calls it)",
+        "image": "https://www.programiz.com/sites/tutorial2program/files/javascript-function-with-parameter.png",
+        "article_link_one": "https://www.w3schools.com/js/js_functions.asp",
+        "article_link_two": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions",
+        "video_link": "https://www.youtube.com/watch?v=xUI5Tsl2JpY"
+    },
+    {
+        "title": "If statements",
+        "definition": "Use if to specify a block of code to be executed, if a specified condition is true. Use else to specify a block of code to be executed, if the same condition is false",
+        "image": "https://cdn.programiz.com/sites/tutorial2program/files/js-if-else-if-statement_0.png",
+        "article_link_one": "https://developer.mozilla.org/en-US/docs/WebAssembly/Reference/Control_flow/if...else",
+        "article_link_two": "https://www.w3schools.com/js/js_if_else.asp",
+        "video_link": "https://www.youtube.com/watch?v=IsG4Xd6LlsM"
+    },
+    {
+        "title": "Object",
+        "definition": "In JavaScript, an object is a standalone entity, with properties and type. Compare it with a cup, for example. A cup is an object, with properties. A cup has a color, a design, weight, a material it is made of, etc. The same way, JavaScript objects can have properties, which define their characteristics.",
+        "image": "https://www.tutorialstonight.com/assets/js/javascript-object.webp",
+        "article_link_one": "https://www.w3schools.com/js/js_object_definition.asp",
+        "article_link_two": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object",
+        "video_link": "https://www.youtube.com/watch?v=BRSg22VacUA"
+    },
+    {
+        "title": "React props",
+        "definition": "In React, the term props stands for properties, which refers to the properties of an object. This is because all elements created in React are JavaScript objects. As a result, passing data into a component is done by creating properties and values for the object.",
+        "image": "https://dmitripavlutin.com/react-props/cover.png",
+        "article_link_one": "https://reactjs.org/docs/components-and-props.html",
+        "article_link_two": "https://www.w3schools.com/react/react_props.asp",
+        "video_link": "https://www.youtube.com/watch?v=PHaECbrKgs0"
+    },
+    {
+        "title": "React useEffect",
+        "definition": "The useEffect Hook allows you to perform side effects in your components. Some images of side effects are: fetching data, directly updating the DOM, and timers. useEffect accepts two arguments. The second argument is optional.",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDv-xlfL62Kg-ChVPiJbd2KMPB235gF40DEg&usqp=CAU",
+        "article_link_one": "https://reactjs.org/docs/hooks-effect.html",
+        "article_link_two": "https://www.w3schools.com/react/react_useeffect.asp",
+        "video_link": "https://www.youtube.com/watch?v=0ZJgIjIuY7U"
+    },
+    {
+        "title": "React useReducer",
+        "definition": "useReducer returns an array that holds the current state value and a dispatch function to which you can pass an action and later invoke it. While this is similar to the pattern Redux uses, there are a few differences. For image, the useReducer function is tightly coupled to a specific reducer.",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMmANtyYZd1vAjtq-xz0EMV1SncmBUe3nTag&usqp=CAU",
+        "article_link_one": "https://reactjs.org/docs/hooks-reference.html",
+        "article_link_two": "https://www.w3schools.com/react/react_usereducer.asp",
+        "video_link": "https://www.youtube.com/watch?v=kK_Wqx3RnHk"
+    },
+    {
+        "title": "React useState",
+        "definition": "The React useState Hook allows us to track state in a function component. State generally refers to data or properties that need to be tracking in an application.",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzRRzwC_9_5jM3vJ43LZC6w_-_KkGckPSC_Q&usqp=CAU",
+        "article_link_one": "https://reactjs.org/docs/hooks-state.html",
+        "article_link_two": "https://www.w3schools.com/react/react_usestate.asp",
+        "video_link": "https://www.youtube.com/watch?v=4pO-HcG2igk"
+    },
+    {
+        "title": "SQL Like operator",
+        "definition": "The LIKE operator is used in a WHERE clause to search for a specified pattern in a column. There are two wildcards often used in conjunction with the LIKE operator: The percent sign (%) represents zero, one, or multiple characters. The underscore sign (_) represents one, single character.",
+        "image": "https://www.programiz.com/sites/tutorial2program/files/sql-like.png",
+        "article_link_one": "https://www.w3schools.com/sql/sql_like.asp",
+        "article_link_two": "https://www.tutorialspoint.com/sql/sql-like-clause.htm",
+        "video_link": "https://www.youtube.com/watch?v=vEt7bDnXlnY"
+    },
+    {
+        "title": "Unit testing",
+        "definition": "In computer programming, unit testing is a software testing method by which individual units of source code—sets of one or more computer program modules together with associated control data, usage procedures, and operating procedures—are tested to determine whether they are fit for use.",
+        "image": "https://miro.medium.com/max/1138/1*ohoa7R6eyI-r9HKSeLmb7w.png",
+        "article_link_one": "https://en.wikipedia.org/wiki/Unit_testing",
+        "article_link_two": "https://www.techtarget.com/searchsoftwarequality/definition/unit-testing#:~:text=Unit%20testing%20is%20a%20software,developers%20and%20sometimes%20QA%20staff.",
+        "video_link": "https://www.youtube.com/watch?v=3kzHmaeozDI"
+    },
+    {
+        "title": "User Experience",
+        "definition": "User Experience refers to the feeling users experience when using a product, application, system, or service. It is a broad term that can cover anything from how well the user can navigate the product, how easy it is to use, how relevant the content displayed is etc.",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQESTBO3fUDgA38gO51TX4Gdj4cNNdrXHgiDA&usqp=CAU",
+        "article_link_one": "https://www.interaction-design.org/literature/topics/ux-design",
+        "article_link_two": "https://en.wikipedia.org/wiki/User_experience",
+        "video_link": "https://www.youtube.com/watch?v=SRec90j6lTY"
+    },
+    {
+        "title": "User Interface",
+        "definition": "The user interface (UI) is the point of human-computer interaction and communication in a device. This can include display screens, keyboards, a mouse and the appearance of a desktop. It is also the way through which a user interacts with an application or a website.",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxgo9YuXuJtZPi1SCj53XN0BDlDNEqwf-vow&usqp=CAU",
+        "article_link_one": "https://www.techtarget.com/searchapparchitecture/definition/user-interface-UI",
+        "article_link_two": "https://en.wikipedia.org/wiki/User_interface",
+        "video_link": "https://www.youtube.com/watch?v=_K06Dni-RE4"
+    },
+    {
+        "title": "While loops",
+        "definition": "A 'While' Loop is used to repeat a specific block of code an unknown number of times, until a condition is met. For example, if we want to ask a user for a number between 1 and 10, we don't know how many times the user may enter a larger number, so we keep asking 'while the number is not between 1 and 10'.",
+        "image": "https://www.tutorialspoint.com/javascript/images/while_loop.jpg",
+        "article_link_one": "https://www.w3schools.com/js/js_loop_while.asp",
+        "article_link_two": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while",
+        "video_link": "https://www.youtube.com/watch?v=leMzF0esu38"
+    }
+];
