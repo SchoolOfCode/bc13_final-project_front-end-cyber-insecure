817aa1182c20a20cd521425349d943a6e2c56e92
constantine30 <banciu.constantin@gmail.com>
2023-01-11 14:06:25 +0000

    icons added

--- src/Components/ApplicationList/Application.js
+++ src/Components/ApplicationList/Application.js
@@ -1,56 +1,88 @@
 import React from "react";
+import {RiDeleteBinLine} from "react-icons/ri"
+import {FiExternalLink} from "react-icons/fi"
+import {AiFillEdit, AiOutlinePlus, AiOutlineMinus} from "react-icons/ai"
+
 
 const progressStage = [
-    {1: "Research"},
-    {2: "Ready to Apply"},
-    {3: "Applied"},
-    {4: "Interview Date"},
-    {5: "Initial Interview Done"},
-    {6: "Second Interview Done"},
-    {7: "Success"},
-]
+  { 1: "Research" },
+  { 2: "Ready to Apply" },
+  { 3: "Applied" },
+  { 4: "Interview Date" },
+  { 5: "Initial Interview Done" },
+  { 6: "Second Interview Done" },
+  { 7: "Success" },
+];
 
 function Application(props) {
   return [
     <div className="application-container">
       <div className="app-sections">
-        <div className="app-section7">
-          <p>Job Title</p>
-          <p2>{props.jobTitle}</p2>
+        <div className="app-section7" id="app-job-title">
+          <div className="app-section-header">
+            <p>Job Title</p>
+          </div>
+          <div className="app-section-footer">
+            <p2>{props.jobTitle}</p2>
+          </div>
         </div>
         <div className="app-section7">
-          <p>Company</p>
-          <p2>{props.company}</p2>
+          <div className="app-section-header">
+            <p>Company</p>
+          </div>
+          <div className="app-section-footer">
+            <p2>{props.company}</p2>
+          </div>
         </div>
         <div className="app-section32">
-          <p>Job Description</p>
-          <p2>{props.jobDescription}</p2>
+          <div className="app-section-header">
+            <p>Job Description</p>
+          </div>
+          <div className="app-section-footer">
+            <p2>{props.jobDescription}</p2>
+          </div>
         </div>
         <div className="app-section7">
-          <p>Location</p>
-          <p2>{props.location}</p2>
+          <div className="app-section-header">
+            <p>Location</p>
+          </div>
+          <div className="app-section-footer">
+            <p2>{props.location}</p2>
+          </div>
         </div>
         <div className="app-section">
-          <p>Salary</p>
-          <p2>{props.salary}</p2>
+          <div className="app-section-header">
+            <p>Salary</p>
+          </div>
+          <div className="app-section-footer">
+            <p2>{props.salary}</p2>
+          </div>
         </div>
         <div className="app-section">
-          <p>Job Link</p>
-          <button
-            onClick={() => {
-              window.open(props.jobLink, "_blank").focus();
-            }}
-          >
-            Open
-          </button>
+          <div className="app-section-header">
+            <p>Job Link</p>
+          </div>
+          <div className="app-section-footer">
+            <button
+              onClick={() => {
+                window.open(props.jobLink, "_blank").focus();
+              }}
+            >
+              <FiExternalLink/>
+            </button>
+          </div>
         </div>
         <div className="app-section32">
-          <p>Notes</p>
-          <p2>{props.notes}</p2>
+          <div className="app-section-header">
+            <p>Notes</p>
+          </div>
+          <div className="app-section-footer">
+            <p2>{props.notes}</p2>
+          </div>
         </div>
         <div className="app-section" id="app-section-buttons">
-          <button>Edit</button>
-          <button>Delete</button>
+          <button><AiFillEdit/></button>
+          <button><RiDeleteBinLine/></button>
         </div>
       </div>
       <div className="app-progress-container">
@@ -60,8 +92,8 @@ function Application(props) {
           </div>
         </div>
         <div className="app-progress-button">
-          <button>-</button>
-          <button>+</button>
+          <button><AiOutlineMinus/></button>
+          <button><AiOutlinePlus/></button>
         </div>
       </div>
     </div>,
