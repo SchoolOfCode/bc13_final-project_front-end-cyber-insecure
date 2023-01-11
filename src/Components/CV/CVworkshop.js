883019a36f1b3e45cda54fd36448a22651a11d12
Barbara Millon <millon.barbara1@gmail.com>
2023-01-11 15:13:33 +0000

    cv workshop buttons + cvs

--- src/Components/CV/CVworkshop.js
+++ src/Components/CV/CVworkshop.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { useState } from 'react';
 import Navbar from '../Navbar/Navbar';
 import frontEndDeveloper from '../../cvImages/frontendDeveloper.png';
+import uxuiPortfolioPage from '../../cvImages/uxuiPortfolioPage.png';
+import backendPortfolio from '../../cvImages/backend.png';
+import fullstackPortfolio from '../../cvImages/fullstack.png';
+
 
 export default function CVworkshop() {
   // Frontend CVs state
@@ -16,19 +20,64 @@ export default function CVworkshop() {
   // UX / UI
   const [uxUi, setUxUi] = useState(false);
 
+
+  function handleFrontEnd() {
+    setFrontEnd(true) 
+      setBackend(false)
+      setFullStack(false)
+      setUxUi(false)
+      
+    
+  }
+
+  function handleFullStack() {
+    setFullStack(true) 
+      setBackend(false)
+      setFrontEnd(false)
+      setUxUi(false)
+    
+  }
+
+  function handleBackend() {
+    setBackend(true) 
+      setFrontEnd(false)
+      setFullStack(false)
+      setUxUi(false)
+    
+  }
+
+  function handleUxUi() {
+    setUxUi(true) 
+      setFrontEnd(false)
+      setBackend(false)
+      setFullStack(false)
+    
+  }
+
+
+
+
   return [
     <Navbar />,
     <h1>This is the CV component</h1>,
     <div className="cvButtonContainer">
-      <button onClick={() => setFrontEnd(true)} className="cvButtonStyles">
+      <button onClick={handleFrontEnd} className="cvButtonStyles">
         Frontend Developer
       </button>
-      <button className="cvButtonStyles">Fullstack Developer</button>
-      <button className="cvButtonStyles">Backend Developer</button>
-      <button className="cvButtonStyles">UX / UI Designer</button>
+      <button onClick={handleFullStack} className="cvButtonStyles">Fullstack Developer</button>
+      <button onClick={handleBackend}className="cvButtonStyles">Backend Developer</button>
+      <button onClick={handleUxUi} className="cvButtonStyles">UX / UI Designer</button>
     </div>,
     <div>
-      {frontEnd && <img src={frontEndDeveloper} alt="your image name " />}
+      {frontEnd && <img src={frontEndDeveloper} alt="frontend portfolio" />}
     </div>,
+    <div>
+      {fullStack && <img src={fullstackPortfolio} alt="fullstack portfolio" />}
+    </div>,
+    <div>{backend && <img src={backendPortfolio} alt="backend portfolio" />}
+    </div>,
+    <div>
+      {uxUi && <img src={uxuiPortfolioPage} alt="ux/ui portfolio" />}
+    </div>
   ];
 }
