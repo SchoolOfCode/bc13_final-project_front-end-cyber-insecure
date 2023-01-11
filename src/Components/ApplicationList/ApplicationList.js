a360a280a9330791bb2a62383c60cc522db92f19
constantine30 <banciu.constantin@gmail.com>
2023-01-11 09:32:54 +0000

    setting up the branch

--- src/Components/ApplicationList/ApplicationList.js
+++ src/Components/ApplicationList/ApplicationList.js
@@ -4,7 +4,7 @@ import Navbar from '../Navbar/Navbar'
 function ApplicationList (props){
     return [
         <Navbar/>,
-        <h1>This is the applications page</h1>
+        <h1>This is the applications page!</h1>
         ]
 }
 export default ApplicationList
