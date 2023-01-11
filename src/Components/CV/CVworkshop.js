6b75ffeb0de7b724a92420fd0a3c17f02ddf9eba
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2023-01-11 17:14:08 +0100

    cvworkshop page basic css done

--- src/Components/CV/CVworkshop.js
+++ src/Components/CV/CVworkshop.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { useState } from 'react';
-import Navbar from '../Navbar/Navbar';
-import frontEndDeveloper from '../../cvImages/frontendDeveloper.png';
-import uxuiPortfolioPage from '../../cvImages/uxuiPortfolioPage.png';
-import backendPortfolio from '../../cvImages/backend.png';
-import fullstackPortfolio from '../../cvImages/fullstack.png';
-
+import React from "react";
+import { useState } from "react";
+import Navbar from "../Navbar/Navbar";
+import frontEndDeveloper from "../../cvImages/frontendDeveloper.png";
+import uxuiPortfolioPage from "../../cvImages/uxuiPortfolioPage.png";
+import backendPortfolio from "../../cvImages/backend.png";
+import fullstackPortfolio from "../../cvImages/fullstack.png";
+import "./CVworkshop.css";
 
 export default function CVworkshop() {
   // Frontend CVs state
@@ -20,64 +20,64 @@ export default function CVworkshop() {
   // UX / UI
   const [uxUi, setUxUi] = useState(false);
 
-
   function handleFrontEnd() {
-    setFrontEnd(true) 
-      setBackend(false)
-      setFullStack(false)
-      setUxUi(false)
-      
-    
+    setFrontEnd(true);
+    setBackend(false);
+    setFullStack(false);
+    setUxUi(false);
   }
 
   function handleFullStack() {
-    setFullStack(true) 
-      setBackend(false)
-      setFrontEnd(false)
-      setUxUi(false)
-    
+    setFullStack(true);
+    setBackend(false);
+    setFrontEnd(false);
+    setUxUi(false);
   }
 
   function handleBackend() {
-    setBackend(true) 
-      setFrontEnd(false)
-      setFullStack(false)
-      setUxUi(false)
-    
+    setBackend(true);
+    setFrontEnd(false);
+    setFullStack(false);
+    setUxUi(false);
   }
 
   function handleUxUi() {
-    setUxUi(true) 
-      setFrontEnd(false)
-      setBackend(false)
-      setFullStack(false)
-    
+    setUxUi(true);
+    setFrontEnd(false);
+    setBackend(false);
+    setFullStack(false);
   }
-
-
-
 
   return [
     <Navbar />,
-    <h1>This is the CV component</h1>,
-    <div className="cvButtonContainer">
-      <button onClick={handleFrontEnd} className="cvButtonStyles">
-        Frontend Developer
-      </button>
-      <button onClick={handleFullStack} className="cvButtonStyles">Fullstack Developer</button>
-      <button onClick={handleBackend}className="cvButtonStyles">Backend Developer</button>
-      <button onClick={handleUxUi} className="cvButtonStyles">UX / UI Designer</button>
+    <div className="cvworkshop-container">
+      <h1>This is the CV component</h1>
+      <div className="cvButtonContainer">
+        <button onClick={handleFrontEnd} className="cvButtonStyles">
+          Frontend Developer
+        </button>
+        <button onClick={handleFullStack} className="cvButtonStyles">
+          Fullstack Developer
+        </button>
+        <button onClick={handleBackend} className="cvButtonStyles">
+          Backend Developer
+        </button>
+        <button onClick={handleUxUi} className="cvButtonStyles">
+          UX / UI Designer
+        </button>
+      </div>
+      <div>
+        {frontEnd && <img src={frontEndDeveloper} alt="frontend portfolio" />}
+      </div>
+      <div>
+        {fullStack && (
+          <img src={fullstackPortfolio} alt="fullstack portfolio" />
+        )}
+      </div>
+      <div>
+        {backend && <img src={backendPortfolio} alt="backend portfolio" />}
+      </div>
+      <div>{uxUi && <img src={uxuiPortfolioPage} alt="ux/ui portfolio" />}</div>
     </div>,
-    <div>
-      {frontEnd && <img src={frontEndDeveloper} alt="frontend portfolio" />}
-    </div>,
-    <div>
-      {fullStack && <img src={fullstackPortfolio} alt="fullstack portfolio" />}
-    </div>,
-    <div>{backend && <img src={backendPortfolio} alt="backend portfolio" />}
-    </div>,
-    <div>
-      {uxUi && <img src={uxuiPortfolioPage} alt="ux/ui portfolio" />}
-    </div>
   ];
 }
