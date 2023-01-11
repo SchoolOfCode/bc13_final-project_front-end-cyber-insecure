2480c48b6be00935b8f1abea3a78c8ab4086deb7
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-01-11 14:13:57 +0000

    Barbara and Taylor CV Portfolio Commit, first commit taylor

--- src/Components/CV/CVworkshop.js
+++ src/Components/CV/CVworkshop.js
@@ -1,9 +1,34 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
+import React from 'react';
+import { useState } from 'react';
+import Navbar from '../Navbar/Navbar';
+import frontEndDeveloper from '../../cvImages/frontendDeveloper.png';
 
 export default function CVworkshop() {
+  // Frontend CVs state
+  const [frontEnd, setFrontEnd] = useState(false);
+
+  // Fullstack CVs state
+  const [fullStack, setFullStack] = useState(false);
+
+  // Backend CVs state
+  const [backend, setBackend] = useState(false);
+
+  // UX / UI
+  const [uxUi, setUxUi] = useState(false);
+
   return [
-    <Navbar/>,
-    <h1>This is the CV component</h1>
-  ]
+    <Navbar />,
+    <h1>This is the CV component</h1>,
+    <div className="cvButtonContainer">
+      <button onClick={() => setFrontEnd(true)} className="cvButtonStyles">
+        Frontend Developer
+      </button>
+      <button className="cvButtonStyles">Fullstack Developer</button>
+      <button className="cvButtonStyles">Backend Developer</button>
+      <button className="cvButtonStyles">UX / UI Designer</button>
+    </div>,
+    <div>
+      {frontEnd && <img src={frontEndDeveloper} alt="your image name " />}
+    </div>,
+  ];
 }
