15e1e17a76f98f9b6f656eaf505ed17597c51041
Callasaurus <c.sullivandrage@outlook.com>
2023-01-12 20:08:29 +0000

    back end hooked up to front end for knowledge bank

--- src/Components/KnowledgeBankList/KnowledgeBankList.js
+++ src/Components/KnowledgeBankList/KnowledgeBankList.js
@@ -1,15 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../Navbar/Navbar'
-import { KnowledgeBank } from './KnowledgeDummyData'
 import KnowledgeBankItem from './KnowledgeBankItem'
 import './KnowledgeBank.css'
 
 export default function KnowledgeBankList() {
 
+  const [knowledge, setKnowledge] = useState([])
   const [input, setInput] = useState('')
   
   function handleChange(e) {
     setInput(e.target.value);
+  }
+
+  useEffect(() => {
+    async function getAllKnowledge() {
+      const titleObject = await fetch(`http://localhost:3001/api/knowledgeBank`);
+      let data = await titleObject.json();
+      setKnowledge(data.payload);
+    }
+    getAllKnowledge()
+  }, [])
+
+  async function getByTitle() {
+    const titleObject = await fetch(`http://localhost:3001/api/knowledgeBank/${input}`);
+    let data = await titleObject.json();
+    setKnowledge(data.payload);
   }
 
   return [
@@ -18,10 +33,10 @@ export default function KnowledgeBankList() {
       <h1>This is the Knowledge Bank component</h1>
       <div className='input-container'>
         <input onChange={handleChange} />
-        <button>Search</button>
+        <button onClick={getByTitle}>Search</button>
       </div>
       <ul className="object-list-container">
-        {KnowledgeBank.map((item) => {
+        {knowledge.map((item) => {
           return (<KnowledgeBankItem key={item.id} title={item.title} definition={item.definition} image={item.image} article_link_one={item.article_link_one} article_link_two={item.article_link_two} video_link={item.video_link} />)
         })}
       </ul>
