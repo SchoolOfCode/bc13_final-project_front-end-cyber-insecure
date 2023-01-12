28d1fef4b543b0f9d5c38e12224a404c6edfe099
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-01-12 10:45:48 +0000

    Taylor and Barbara commit, cv component page

--- src/Components/CV/CVworkshop.js
+++ src/Components/CV/CVworkshop.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { useState } from "react";
-import Navbar from "../Navbar/Navbar";
-import frontEndDeveloper from "../../cvImages/frontendDeveloper.png";
-import uxuiPortfolioPage from "../../cvImages/uxuiPortfolioPage.png";
-import backendPortfolio from "../../cvImages/backend.png";
-import fullstackPortfolio from "../../cvImages/fullstack.png";
-import "./CVworkshop.css";
+import React from 'react';
+import { useState } from 'react';
+import Navbar from '../Navbar/Navbar';
+import frontEndDeveloper from '../../cvImages/frontendDeveloper.png';
+import uxuiPortfolioPage from '../../cvImages/uxuiPortfolioPage.png';
+import backendPortfolio from '../../cvImages/backend.png';
+import fullstackPortfolio from '../../cvImages/fullstack.png';
+import './CVworkshop.css';
 
 export default function CVworkshop() {
   // Frontend CVs state
@@ -51,7 +51,7 @@ export default function CVworkshop() {
   return [
     <Navbar />,
     <div className="cvworkshop-container">
-      <h1>This is the CV component</h1>
+      <h1>CV Templates and Suggestions</h1>
       <div className="cvButtonContainer">
         <button onClick={handleFrontEnd} className="cvButtonStyles">
           Frontend Developer
@@ -68,16 +68,30 @@ export default function CVworkshop() {
       </div>
       <div className="cvExamples">
         {frontEnd && <img src={frontEndDeveloper} alt="frontend portfolio" />}
+        {frontEnd && <img src={frontEndDeveloper} alt="frontend portfolio" />}
+        {frontEnd && <img src={frontEndDeveloper} alt="frontend portfolio" />}
       </div>
       <div className="cvExamples">
+        {fullStack && (
+          <img src={fullstackPortfolio} alt="fullstack portfolio" />
+        )}
+        {fullStack && (
+          <img src={fullstackPortfolio} alt="fullstack portfolio" />
+        )}
         {fullStack && (
           <img src={fullstackPortfolio} alt="fullstack portfolio" />
         )}
       </div>
       <div className="cvExamples">
         {backend && <img src={backendPortfolio} alt="backend portfolio" />}
+        {backend && <img src={backendPortfolio} alt="backend portfolio" />}
+        {backend && <img src={backendPortfolio} alt="backend portfolio" />}
       </div>
-      <div className="cvExamples">{uxUi && <img src={uxuiPortfolioPage} alt="ux/ui portfolio" />}</div>
+      <div className="cvExamples">
+        {uxUi && <img src={uxuiPortfolioPage} alt="ux/ui portfolio" />}
+        {uxUi && <img src={uxuiPortfolioPage} alt="ux/ui portfolio" />}
+        {uxUi && <img src={uxuiPortfolioPage} alt="ux/ui portfolio" />}
+      </div>
     </div>,
   ];
 }
