4da5ff33e83308934672f577f76d426eaefd82ff
Callasaurus <c.sullivandrage@outlook.com>
2023-01-14 23:59:24 +0000

    refactoring

--- src/Components/ApplicationList/ApplicationList.js
+++ src/Components/ApplicationList/ApplicationList.js
@@ -9,13 +9,11 @@ import { useAuth } from "../contexts/AuthContext";
 
 const url = process.env.REACT_APP_BACKEND_URL
 
-function ApplicationList(props) {
+function ApplicationList() {
 
   const [adding, setAdding] = useState()
   const [applications, setApplications] = useState([])
   const { currentUser } = useAuth();
-
-  console.log(applications)
 
   // useEffect calls below getAllApplications function 
   useEffect(() => {
@@ -41,52 +39,45 @@ function ApplicationList(props) {
         await fetch(`${url}/api/jobApplications/${id}`, {
           method: "DELETE"
         })
-        const deleted = [...applications.slice(0, i), ...applications.slice(i + 1)];
-        setApplications(deleted);
       }
-    } return
+    }
+    getAllApplications();
   }
 
   // PATCH request adds 1 to the progress bar. Need to first fetch the data to get the correct progress, edit the progress, and then fetch the data again so it is shown on screen
   async function handleEditAddProgress(id, application) {
-    await getAllApplications();
+    getAllApplications();
     for (let i = 0; i < applications.length; i++) {
       if (applications[i].id === id) {
         application = applications[i]
       }
     }
     if (application.progress < 7) {
-      for (let i = 0; i < applications.length; i++) {
-        if (applications[i].id === id) {
-          await fetch(`${url}/api/jobApplications/${id}`, {
-            method: "PATCH",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ progress: application.progress + 1 })
-          })
-        }
-      }
-    } await getAllApplications();
+      await fetch(`${url}/api/jobApplications/${id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ progress: application.progress + 1 })
+      })
+    }
+    getAllApplications();
   }
 
   // PATCH request removes 1 from the progress bar. Need to first fetch the data to get the correct progress, edit the progress, and then fetch the data again so it is shown on screen
   async function handleEditRemoveProgress(id, application) {
-    await getAllApplications();
+    getAllApplications();
     for (let i = 0; i < applications.length; i++) {
       if (applications[i].id === id) {
         application = applications[i]
       }
     }
     if (application.progress > 1) {
-      for (let i = 0; i < applications.length; i++) {
-        if (applications[i].id === id) {
-          await fetch(`${url}/api/jobApplications/${id}`, {
-            method: "PATCH",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ progress: application.progress - 1 })
-          })
-        }
-      }
-    }  getAllApplications();
+      await fetch(`${url}/api/jobApplications/${id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ progress: application.progress - 1 })
+      })
+    }
+    getAllApplications();
   }
 
   // function that sends a POST request - sent to Form component and runs when form button is clicked. Only runs when adding state is TRUE
@@ -97,6 +88,25 @@ function ApplicationList(props) {
       body: JSON.stringify(newApplication)
     })
     getAllApplications();
+  }
+
+  // function for sorting - onClick not allowed on option tags so have to use onChange on the select tag and use the value to then run the sort and update the state
+  function sortFunction(e) {
+    if (e.target.value === "Progress high-low") {
+      let highProgress = [...applications].sort((a, b) => b.progress - a.progress)
+      setApplications(highProgress)
+    } if (e.target.value === "Progress low-high") {
+      let lowProgress = [...applications].sort((a, b) => a.progress - b.progress)
+      setApplications(lowProgress)
+    }
+    // have to do parseInt on salary values as they are a string
+    if (e.target.value === "Salary high-low") {
+      let highProgress = [...applications].sort((a, b) => parseInt(b.salary) - parseInt(a.salary))
+      setApplications(highProgress)
+    } if (e.target.value === "Salary low-high") {
+      let lowProgress = [...applications].sort((a, b) => parseInt(a.salary) - parseInt(b.salary))
+      setApplications(lowProgress)
+    }
   }
 
   return [
@@ -118,27 +128,28 @@ function ApplicationList(props) {
               {(close) => (
                 <div className="app-popup-container">
                   <div className="app-popup">
-                    <Form 
-                    adding={adding}
-                    handleAddNewApplication={handleAddNewApplication}
-                    getAllApplications={getAllApplications}
-                    close={close} />
+                    <Form
+                      adding={adding}
+                      handleAddNewApplication={handleAddNewApplication}
+                      getAllApplications={getAllApplications}
+                      close={close} />
                   </div>
                 </div>
               )}
             </Popup>
           </div>
           <div className="app-buttons-right">
-            <select>
+            <select onChange={sortFunction}>
               <option value="">Sort By</option>
-              <option value="">Salary high-low</option>
-              <option value="">Salary low-high</option>
-              <option value="">Progress high-low</option>
-              <option value="">Progress low-high</option>
+              <option value="Salary high-low">Salary high-low</option>
+              <option value="Salary low-high">Salary low-high</option>
+              <option value="Progress high-low">Progress high-low</option>
+              <option value="Progress low-high">Progress low-high</option>
             </select>
             <select>
               <option value="">Display All</option>
               <option value="">Research Stage</option>
+              <option value="">Ready to Apply</option>
               <option value="">Applied</option>
               <option value="">Interview Date</option>
               <option value="">Initial Interview Done</option>
@@ -148,33 +159,33 @@ function ApplicationList(props) {
           </div>
         </div>
       </div>
-      {applications.length < 1 && 
-      <h1>No current applications</h1>
+      {applications.length < 1 &&
+        <h1>No current applications</h1>
       }
-      {applications && 
-      <ul className="applications-main">
-        {applications.map((app) => {
-          return (
-            <Application
-              getAllApplications={getAllApplications}
-              handleDelete={() => handleDelete(app.id)}
-              handleEditAddProgress={() => handleEditAddProgress(app.id, app)}
-              handleEditRemoveProgress={() => handleEditRemoveProgress(app.id, app)}
-              id={app.id}
-              adding={app}
-              setAdding={setAdding}
-              job_title={app.job_title}
-              company={app.company}
-              job_description={app.job_description}
-              location={app.location}
-              salary={app.salary}
-              job_link={app.jobLink}
-              notes={app.notes}
-              progress={app.progress}
-            />
-          );
-        })}
-      </ul>
+      {applications &&
+        <ul className="applications-main">
+          {applications.map((app) => {
+            return (
+              <Application
+                getAllApplications={getAllApplications}
+                handleDelete={() => handleDelete(app.id)}
+                handleEditAddProgress={() => handleEditAddProgress(app.id, app)}
+                handleEditRemoveProgress={() => handleEditRemoveProgress(app.id, app)}
+                id={app.id}
+                adding={app}
+                setAdding={setAdding}
+                job_title={app.job_title}
+                company={app.company}
+                job_description={app.job_description}
+                location={app.location}
+                salary={app.salary}
+                job_link={app.job_link}
+                notes={app.notes}
+                progress={app.progress}
+              />
+            );
+          })}
+        </ul>
       }
     </div>,
   ];
