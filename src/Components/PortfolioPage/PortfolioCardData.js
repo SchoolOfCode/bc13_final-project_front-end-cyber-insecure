db11adc79059ad3178fe5cdc8cbe7c0af251cb0a
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2023-01-17 20:16:49 +0100

    portfolio carousel works to left only - bug

---
+++ src/Components/PortfolioPage/PortfolioCardData.js
@@ -0,0 +1,51 @@
+// import jordanGithubPlain from "../../Images/jordanGithubPlain.png";
+// import jordanGithubPopup from "../../Images/jordanGithubPopup.png";
+// import jordanPortfolioPlain from "../../Images/jordanPortfolioPlain.png";
+// import jordanPortfolioPopup from "../../Images/jordanPorfolioPopup.png";
+// import julioGithubPlain from "../../Images/julioGithubPlain.png";
+// import julioGithubPopup from "../../Images/julioGithubPopup.png";
+// import julioPortfolioPlain from "../../Images/julioPortfolioPlain.png";
+// import julioPortfolioPopup from "../../Images/julioPortfolioPopup.png";
+// import aakashGithubPlain from "../../Images/aakashGithubPlain.png";
+// import aakashGithubPopup from "../../Images/aakashGithubPopup.png";
+// import aakashPortfolioPlain from "../../Images/aakashPortfolioPlain.png";
+// import aakashPortfolioPopup from "../../Images/aakashPortfolioPopup.png";
+
+export let portfolioPageData = [
+  {
+    headerH2: "GitHub page",
+    iconLink: "https://github.com/Jordaneddielinton93",
+    plainSrc: "../../Images/jordanGithubPlain.png",
+    popSrc: "../../Images/jordanGithubPopup.png",
+  },
+  {
+    headerH2: "Portfolio page",
+    iconLink: "https://jordansfreelancing.co.uk/",
+    plainSrc: "../../Images/jordanPortfolioPlain.png",
+    popSrc: "../../Images/jordanPorfolioPopup.png",
+  },
+  {
+    headerH2: "GitHub page",
+    iconLink: "https://github.com/Lakorthus",
+    plainSrc: "../../Images/julioGithubPlain.png",
+    popSrc: "../../Images/julioGithubPopup.png",
+  },
+  {
+    headerH2: "Portfolio page",
+    iconLink: "https://react-portafolio-two.vercel.app/",
+    plainSrc: "../../Images/julioPortfolioPlain.png",
+    popSrc: "../../Images/julioPortfolioPopup.png",
+  },
+  {
+    headerH2: "GitHub page",
+    iconLink: "https://github.com/aakashsh1999",
+    plainSrc: "../../Images/aakashGithubPlain.png",
+    popSrc: "../../Images/aakashGithubPopup.png",
+  },
+  {
+    headerH2: "Portfolio page",
+    iconLink: "https://aakash-sharma.netlify.app/",
+    plainSrc: "../../Images/aakashPortfolioPlain.png",
+    popSrc: "../../Images/aakashPortfolioPopup.png",
+  },
+];
