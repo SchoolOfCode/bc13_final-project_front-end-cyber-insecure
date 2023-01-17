20b101f971f613b87fde9f2111c68b2c3d7e965f
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-01-17 11:56:23 +0000

    Taylor commit copied Constantines code for dummy data

--- src/Components/InterviewPrepPage/Card/Card.js
+++ src/Components/InterviewPrepPage/Card/Card.js
@@ -1,31 +1,26 @@
-import React from "react";
-import Popup from "reactjs-popup";
-import "../InterviewPrepPage.css"
-
+import React from 'react';
+import Popup from 'reactjs-popup';
+import '../InterviewPrepPage.css';
 
 function Card(props) {
   return (
-    <div className="questionStyling">
-      <div className="">
+    <div className="cardStyling">
+      <div className="question">
         <h3>{props.question}</h3>
-        <br />
-        <br />
       </div>
-      <div>
+      <div className="answer-button">
         <Popup
           trigger={<button className="exampleButton">Answer Example</button>}
         >
           {(close) => (
             <div className="popup">
               <div className="popup-inner">
-                <p>
-                {props.answer}
-                </p>
+                <p>{props.answer}</p>
                 {/* <button className="close-btn" onClick={() => props.setTrigger(false)}>Close</button>  */}
                 <button
                   className="button"
                   onClick={() => {
-                    console.log("modal closed ");
+                    console.log('modal closed ');
                     close();
                   }}
                 >
