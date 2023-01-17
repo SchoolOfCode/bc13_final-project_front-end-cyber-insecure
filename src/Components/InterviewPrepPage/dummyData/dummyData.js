6a937160b85e6c0b1ab77ce1a998cbb9e49a84ee
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-01-17 11:52:20 +0000

    Taylor commit copied Constantines code for dummy data

--- src/Components/InterviewPrepPage/dummyData/dummyData.js
+++ src/Components/InterviewPrepPage/dummyData/dummyData.js
@@ -1,54 +1,98 @@
 const interviewQuestions = [
-  {
-    question:
-      'Can you tell us about your programming experience and background?',
-    answer:
-      'I have been studying software developer for {your answer } and have experience in programming languages such as {your tech }. I have also completed several projects such as a web scraping tool and a small game using Java."
-  },
+  // General Interview Questions
 
-  {
-    question:
-      ' Do you like to work in a team? What would be a perfect team for you?',
-    answer: `I think it's important for developers to be able to work
+  [
+    {
+      question: 'Why did you choose to be a developer?',
+      answer: `As a developer, I have a passion for using technology
+          to solve problems and improve people's lives.
+          <br />I enjoy the process of creating and building
+          software, from understanding user needs and defining
+          requirements, to designing and implementing solutions,
+          to testing and maintaining the final product.
+          <br />I find it particularly satisfying to see the
+          impact of my work on users and the positive difference
+          it can make in their lives.
+          <br />
+          Additionally, I am constantly learning new
+          technologies, methodologies, and best practices which
+          make the job challenging and enjoyable.`,
+    },
+
+    {
+      question: ` Do you like to work in a team? What would be a perfect team for you?`,
+      answer: `I think it's important for developers to be able to work
     well in teams.
-    <br />
+    
     Working in a team allows for different perspectives and
     ideas to be shared, which can lead to better solutions.
-    <br />
+    
     It also allows for more efficient problem-solving, as
     team members can divide up the work and leverage each
     other's strengths.
-    <br />
+    
     Additionally, working in a team environment can be more
     enjoyable and motivating than working alone.
-    <br />
+    
     So it is important for a developer to be able to
     communicate effectively and be able to work together
     with others.`,
-  },
+    },
 
-  {
-    question: 'What is your motivation for joining the @companyname?',
-    answer: `Express your excitement about the company's mission,
+    {
+      question: `What is your motivation for joining the @companyname?`,
+      answer: `Express your excitement about the company's mission,
     values, and industry leadership.
-    <br />
-    <br />
+   
     You may also highlight specific projects or initiatives
     that the company is working on that align with your own
     interests and experience.
-    <br />
-    <br />
+    
     Additionally, you may discuss how you believe your
     skills and experiences can contribute to the company's
     success and how you are looking forward to being a part
     of the team.
-    <br />
-    <br />
+    
     Finally you may express that you want to work with the
     team members and grow your skills, and that you
     appreciate the opportunities that the company can offer
     you to achieve your professional and personal goals.`,
-  },
+    },
+  ],
+
+  // Technical Questions
+
+  [
+    {
+      question: 'Technical Question',
+      answer: 'Technical Answer',
+    },
+    {
+      question: '',
+      answer: '',
+    },
+    {
+      question: '',
+      answer: '',
+    },
+  ],
+
+  // Questions to ask after the Interview
+
+  [
+    {
+      question: 'What is the work culture in your company?',
+      answer: 'You work and you get paid!',
+    },
+    {
+      question: '',
+      answer: '',
+    },
+    {
+      question: '',
+      answer: '',
+    },
+  ],
 ];
 
 export default interviewQuestions;
