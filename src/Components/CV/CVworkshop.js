b45f9f461ed004bd8c249bf3b2931aca84b142fc
Callasaurus <c.sullivandrage@outlook.com>
2023-01-18 10:52:03 +0000

    cv

--- src/Components/CV/CVworkshop.js
+++ src/Components/CV/CVworkshop.js
@@ -45,31 +45,70 @@ export default function CVworkshop() {
             </div>
             {CVonScreen === frontEndDeveloper &&
               <div className='individual-cv-right'>
-                <p className='cv-p'> FRONTEND Placeholder text (why the CV is good) </p>
+                <h2 className='cv-p'> Front-end CV breakdown </h2>
+                <h3>Objective / Personal Statement</h3>
+                <p className='cv-p'> This CV is a good example for other aspiring junior full-stack developers because it demonstrates a range of experiences and skills that are relevant to the role. For example, in the objective/personal statement, the applicant mentions their passion for coding, their experience attending a coding bootcamp, and how they are able to collaborate with senior developers and take on extra responsibilities when necessary.
+                </p>
+                <h3>Skills</h3>
+                <p className='cv-p'>
+                  In terms of skills, the applicant includes a range of technical skills such as HTML, CSS, React, Node.js, Mocha/Chai, Jest, and GIT/GitHub. This shows that they have a good understanding of the tools and technologies used in full-stack development.
+                </p>
+                <h3>Work Experience</h3>
+                <p className='cv-p'>
+                  The work experience section details two specific positions the applicant held that are relevant to full-stack development. These include tasks such as setting up a testing framework, working in an agile environment, reviewing code, and developing user interfaces. This shows that the applicant has had the opportunity to apply the skills they have learned in a professional setting. Overall, this CV is a good example for aspiring junior full-stack developers because it demonstrates a range of experiences and skills that are relevant to the role. </p>
                 <button className="cvButtonStyles" onClick={() => window.open(require('../../CV-pdfs/frontendcv.pdf'), '_none')}>View and download</button>
               </div>
             }
             {CVonScreen === fullstackPortfolio &&
               <div className='individual-cv-right'>
-                <p className='cv-p'> FULLSTACK Placeholder text (why the CV is good) </p>
+                <h2 className='cv-p'> Full-stack CV breakdown </h2>
+                <h3>Objective / Personal Statement</h3>
+                <p>This is a good example for other aspiring junior full-stack developers because it illustrates a
+                  number of important aspects of a successful CV. The objective/personal statement clearly states
+                  the applicant's relevant skills and experience, as well as their passion for developing web applications.</p>
+                <h3>Skills</h3>
+                <p>The skills section is comprehensive and includes the most relevant technologies for a full-stack developer.</p>
+                <h3>Work experience</h3>
+                <p>Finally the work experience section provides concrete examples of the applicant's accomplishments, making it
+                  easy for a potential employer to see the value they could bring to their organization.</p>
                 <button className="cvButtonStyles" onClick={() => window.open(require('../../CV-pdfs/fullstackcv.pdf'), '_none')}>View and download</button>
               </div>
             }
             {CVonScreen === backendPortfolio &&
               <div className='individual-cv-right'>
-                <p className='cv-p'> BACKEND Placeholder text (why the CV is good) </p>
+                <h2 className='cv-p'> Back-end CV breakdown </h2>
+                <h3>Objective / Personal Statement</h3>
+                <p>This CV is a good example for other aspiring junior back-end developers because it provides a comprehensive
+                  overview of the candidate's skills, experience, and objective. The objective/personal statement is a good example
+                  because it succinctly summarizes the candidate's experience, skills, and passion for software development. </p>
+                <h3>Skills</h3>
+                <p>The skills section is a good example because it provides a detailed list of the candidate's technical capabilities and software knowledge. </p>
+                <h3>Work experience</h3>
+                <p>The work experience section is a good example because it provides a detailed overview of the candidate's professional experience, including specific
+                  tasks they have been responsible for and the technologies they have used.</p>
                 <button className="cvButtonStyles" onClick={() => window.open(require('../../CV-pdfs/backendcv.pdf'), '_none')}>View and download</button>
               </div>
             }
             {CVonScreen === uxuiPortfolioPage &&
               <div className='individual-cv-right'>
-                <p className='cv-p'> UI/UX Placeholder text (why the CV is good) </p>
-                <button className="cvButtonStyles" onClick={() => window.open(require('../../CV-pdfs/uiuxcv.pdf'), '_none')}>View and download</button>
+                <h2 className='cv-p' >UI / UX CV breakdown</h2>
+                <h3>Objective / Personal Statement</h3>
+                <p className='cv-p'> This CV is a good example for other aspiring UX/UI designers because it provides an overview of the applicant's experience, skills, and education. The objective/personal statement clearly states the applicant’s goal in seeking a position in an environment that will challenge them while allowing them to contribute to the success of the organization. 
+                </p>
+                <h3>Skills</h3>
+                <p className='cv-p'>
+                  The skills section lists relevant UX/UI design skills including user experience, user interface design, graphic design, and visual design. 
+                </p>
+                <h3>Work Experience</h3>
+                <p className='cv-p'>
+                  The work experience section provides detailed descriptions of the applicant’s responsibilities and accomplishments in previous roles, as well as a list of the tools and technologies used.
+                </p>
+                 <button className="cvButtonStyles" onClick={() => window.open(require('../../CV-pdfs/uiuxcv.pdf'), '_none')}>View and download</button>
               </div>
-            }
-          </div>
         }
       </div>
-    </div>,
+        }
+    </div>
+    </div >,
   ];
 }
